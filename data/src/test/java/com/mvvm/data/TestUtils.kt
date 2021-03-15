90f4118a24c312c1030183a6aa88495c36e26db4
Dharmendra <dharam.shoekonnect@gmail.com>
2021-03-15 13:10:46 +0530

    added sonarqube with sonarcloud for static code analysis

--- data/src/test/java/com/mvvm/data/TestUtils.kt
+++ data/src/test/java/com/mvvm/data/TestUtils.kt
@@ -4,6 +4,7 @@ import com.mvvm.data.database.model.WeatherEntity
 import com.mvvm.data.networking.model.MainInfo
 import com.mvvm.data.networking.model.WeatherInfoResponse
 import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.ResponseBody
 
 const val CITY_NAME = "Delhi"
@@ -11,5 +12,5 @@ const val FAKE_FAILURE_ERROR_CODE = 400
 const val UPDATED_CITY_NAME = "London"
 
 val successWeatherInfoResponse = WeatherInfoResponse(0, arrayListOf(), MainInfo(), CITY_NAME)
-val failureResponseBody = ResponseBody.create(MediaType.parse("text"), "network error")
+val failureResponseBody = ResponseBody.create("text".toMediaTypeOrNull(), "network error")
 val fakeWeatherEntity = WeatherEntity(0, arrayListOf(), MainInfo(), UPDATED_CITY_NAME)
